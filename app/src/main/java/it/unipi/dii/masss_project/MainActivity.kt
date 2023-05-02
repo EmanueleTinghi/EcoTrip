c73bfa9eb852d93b89db211e35d3711d97559d52
Federico Montini <74728075+FedericoMontini98@users.noreply.github.com>
2023-05-02 10:53:29 +0200

    Added Recording Activity
    Added login functionality and second  activity "RecordingActivity"
    
    Co-Authored-By: EmanueleTinghi <92534978+EmanueleTinghi@users.noreply.github.com>
    Co-Authored-By: Fedeperrone98 <92581085+Fedeperrone98@users.noreply.github.com>
    Co-Authored-By: Veronica Torraca <80213270+veronicator@users.noreply.github.com>

--- app/src/main/java/it/unipi/dii/masss_project/MainActivity.kt
+++ app/src/main/java/it/unipi/dii/masss_project/MainActivity.kt
@@ -1,11 +1,21 @@
 package it.unipi.dii.masss_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.loginButton)
+        button.setOnClickListener {
+            val intent = Intent(this, RecordingActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
