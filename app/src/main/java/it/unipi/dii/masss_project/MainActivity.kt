00bac72521d64bf11a718ebb336f491721352975
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-05-03 11:41:22 +0200

    start login functionality

--- app/src/main/java/it/unipi/dii/masss_project/MainActivity.kt
+++ app/src/main/java/it/unipi/dii/masss_project/MainActivity.kt
@@ -6,9 +6,11 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import it.unipi.dii.masss_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(
@@ -20,14 +22,34 @@ class MainActivity : AppCompatActivity() {
 
     private fun onLoginAttempt(binding: ActivityMainBinding) {
         val intent = Intent(this, RecordingActivity::class.java)
-        // Pass the inserted username to the intent
+
         val username: String = binding.inputUsername.text.toString()
         val email: String = binding.inputEmail.text.toString()
         val password: String = binding.inputPassword.text.toString()
+
         if(username != "" || password != "" || email!= "") {
-            intent.putExtra("username", username)
-            startActivity(intent)
             // TODO: Aggiungi login a Firebased (Database)
+            val auth: FirebaseAuth = FirebaseAuth.getInstance()
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        var user = auth.currentUser
+
+                        // Pass the inserted username to the intent
+                        intent.putExtra("username", username)
+                        startActivity(intent)
+                    } else {
+                        // Otherwise show error message
+                        val message = "Authentication failed: ${task.exception}"
+                        val duration = Toast.LENGTH_LONG
+
+                        val toast = Toast.makeText(this, message, duration)
+                        toast.show()
+                    }
+                }
+
+
+
         }
         else{
             // Otherwise show error message
