0df2903d10a4ea83801101e43da7de5c432c4102
Emanuele Tinghi <ema.tinghi@gmail.com>
2023-05-04 11:01:54 +0200

    added gyroscope code

--- app/src/main/java/it/unipi/dii/masss_project/Sensors.kt
+++ app/src/main/java/it/unipi/dii/masss_project/Sensors.kt
@@ -1,13 +1,61 @@
 package it.unipi.dii.masss_project
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.media.MediaRecorder
 
-class SensorGyroscope : SensorEventListener {
-    
+class SensorGyroscope() : SensorEventListener {
+    private val gyroscopeSensor: Sensor? = null
+    private val orientation: FloatArray = FloatArray(3)
+    private val angularVelocity: FloatArray = FloatArray(3)
+    private val acceleration: FloatArray = FloatArray(3)
+    private val isRunning: Boolean = false
+    private val samplingInterval : Long = 1000 // 1 second
+    private var lastUpdateAccelerometer: Long = 0
+    fun getOrientation(): FloatArray{
+        return orientation.clone();
+    }
+
+    fun getAngularVelocity(): FloatArray{
+        return angularVelocity.clone();
+    }
+
+    fun getAcceleration(): FloatArray{
+        return acceleration.clone()
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        //TODO test
+        if (event != null && event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            val currentTime = System.currentTimeMillis()
+
+            // Only process one sample per second
+            if (currentTime - lastUpdateAccelerometer > this.samplingInterval) {
+                lastUpdateAccelerometer = currentTime
+                for (element in event.values) {
+                    println("Gyroscope data: $element")
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        if (sensor != null) {
+            if (accuracy == SensorManager.SENSOR_STATUS_ACCURACY_HIGH) {
+                // Sensor data is highly accurate
+                println("Accuracy high!")
+            } else if (accuracy == SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM) {
+                // Sensor data is moderately accurate
+                println("Accuracy moderately accurate!")
+            } else if (accuracy == SensorManager.SENSOR_STATUS_ACCURACY_LOW) {
+                // Sensor data is less accurate
+                println("Accuracy low!")
+
+            }
+        }
+    }
 }
 
 class SensorAccelerometer : SensorEventListener {
